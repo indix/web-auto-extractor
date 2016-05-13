98848a5539efc115be79ca6680c79dbc2f7b4330
Abdud Dayan Adeeb <addnab@gmail.com>
2016-05-13 14:28:07 +0530

    Microdata parser first cut

--- src/parsers/microdata-parser.js
+++ src/parsers/microdata-parser.js
@@ -1,7 +1,57 @@
-import cheerio from 'cheerio'
+import $ from 'cheerio'
+import md5 from 'md5'
 
-export let getElementsWithMicrodata = function (html) {
-  let $ = cheerio.load(html)
-  let a = $('[itemscope]')
-  return a
+function getMicrodataPropValue (itemPropElement) {
+  if ($(itemPropElement).attr('content')) {
+    return $(itemPropElement).attr('content')
+  } else if ($(itemPropElement).attr('itemprop') === 'image' && $(itemPropElement).attr('src')) {
+    return $(itemPropElement).attr('src')
+  } else {
+    return $(itemPropElement).text()
+  }
+}
+
+function identifyMicrodataItemTypes ($html) {
+  let itemTypes = {}
+  $html('[itemtype]').each((index, itemTypeElement) => {
+    const itemTypeId = md5($(itemTypeElement).html())
+    itemTypes[itemTypeId] = {
+      name: $(itemTypeElement).attr('itemtype'),
+      properties: []
+    }
+  })
+  return itemTypes
+}
+
+function identifyMicrodataItemProps ($html, itemTypes) {
+  let itemProps = {}
+  $html('[itemprop]').each((index, itemPropElement) => {
+    const parentItemType = $(itemPropElement).closest('[itemtype]')
+    const parentItemTypeHtml = $(parentItemType).html()
+    const parentItemTypeId = (parentItemTypeHtml) ? md5($(parentItemType).html()) : null
+    const itemPropId = md5($(itemPropElement).html())
+    if (parentItemTypeId === itemPropId) {
+      itemTypes[itemPropId].isProp = true
+      itemTypes[itemPropId].value = itemPropId
+    } else if (parentItemTypeId) {
+      itemTypes[parentItemTypeId].properties.push(itemPropId)
+    }
+    itemProps[itemPropId] = {
+      name: $(itemPropElement).attr('itemprop'),
+      value: getMicrodataPropValue(itemPropElement),
+      parentItemTypeId
+    }
+  })
+  return {
+    itemTypes,
+    itemProps
+  }
+}
+
+export function parseMicrodataItems (html) {
+  let $html = $.load(html)
+  let microdataItemTypes = identifyMicrodataItemTypes($html)
+  let microdata = identifyMicrodataItemProps($html, microdataItemTypes)
+
+  return microdata
 }
--- test/parsers/microdata.test.js
+++ test/parsers/microdata.test.js
@@ -1,11 +1,12 @@
 /* eslint-env mocha */
-import { getElementsWithMicrodata } from '../../src/parsers/microdata-parser'
+import { parseMicrodataItems } from '../../src/parsers/microdata-parser'
 import fs from 'fs'
 
 describe('Microdata Parser', function () {
   it('should find all elements with microdata', function () {
     const html = fs.readFileSync('test/resources/sample.html')
-    const elem = getElementsWithMicrodata(html)
+    const elem = parseMicrodataItems(html)
+    console.log(JSON.stringify(elem, null, 2))
     return elem
   })
 })
