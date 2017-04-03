8e3dedbbf3a546cdc8ed7f616a41deebd4fa7c90
Vasanth Gopal <vasanth@indix.com>
2017-04-03 10:58:07 +0530

    Remove lodash and replace with native methods. (#15)

--- src/parsers/metatag-parser.js
+++ src/parsers/metatag-parser.js
@@ -1,9 +1,8 @@
-import _ from 'lodash'
 
 export default ($) => {
   let metatagsData = {}
   $('meta').each((index, elem) => {
-    const nameKey = _.find(_.keys(elem.attribs), attr => [ 'name', 'property', 'itemprop' ].indexOf(attr) !== -1)
+    const nameKey = Object.keys(elem.attribs).find((attr) => [ 'name', 'property', 'itemprop' ].indexOf(attr) !== -1)
     const name = elem.attribs[nameKey]
     const value = elem.attribs['content']
     if (!metatagsData[name]) {
